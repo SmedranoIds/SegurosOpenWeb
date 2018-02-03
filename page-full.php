6d847ace6c9a6856b0c1498c3df927fea0c62c37
Aida Rueda Medina <aidarueda.82@gmail.com>
2018-02-02 19:53:11 -0600

    Ajuste de imagen en cards - link agregado

--- page-full.php
+++ page-full.php
@@ -182,7 +182,7 @@ while (have_posts()) {
 					?>
 					<div class="col-md-4">
 						<div class="card" style="width: 28rem;">
-							<img class="card-block" src="<?php the_sub_field('imagecard') ?>" alt="Card image cap">
+						<a href="<?php the_sub_field('urlcardbutton') ?>" target="<?php the_sub_field('targetcardbutton	')?>"><img class="card-block" src="<?php the_sub_field('imagecard') ?>" alt="Card image cap"></a>
 							<div class="card-body">
 								<h5 class="card-title"><?php the_sub_field('titulocard') ?></h5>
 								<p class="card-text"><?php the_sub_field('resumencard') ?></p>
