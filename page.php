710f9fd78456a8b750a6468431d6dc1a6bad4d68
RubenM <rmoralesl@ids.com.mx>
2018-06-04 12:53:58 -0500

    Ajuste en custom field para la url de los iconos del home

--- page.php
+++ page.php
@@ -93,7 +93,15 @@ get_header();
 			?>	
 			<div class="col-md-2 col-xs-4">
 				<div style="text-align: center; padding-bottom: 15px;">
-					<img style="width: 120px;" src="<?php the_sub_field('icono_sec'); ?>" target="<?php the_sub_field('target_icono'); ?>'">
+
+					<?php if ( get_sub_field('url_icono_ext')): ?>
+                     <!-- Si la url es externa, que enlace acepte y use el link externo -->
+                        <a href="<?php the_sub_field('url_icono_ext'); ?>" target="<?php the_sub_field('target_icono'); ?>"><img style="width: 120px;" src="<?php the_sub_field('icono_sec'); ?>"></a>
+                    <?php else :?>
+                    <!-- Si no lo es, que muestre un enlace normal interno -->
+                        <a href="<?php the_sub_field('url_icono'); ?>" target="<?php the_sub_field('target_icono'); ?>"><img style="width: 120px;" src="<?php the_sub_field('icono_sec'); ?>"></a>
+                    <?php endif ;?>
+
 					<label class="label-sectIcon" style="font-size: 1.3rem; margin-top: 10px; width: 100%;"><?php the_sub_field('label_icono'); ?></label>
 				</div>
 			</div>
