c69909f2e6a3e6d0fe2cc1bdc6a296bebd7fb05b
RubenM <rmoralesl@ids.com.mx>
2018-06-04 17:08:01 -0500

    cambio de iconografía en el home page y ajuste de padding en el menú

--- page.php
+++ page.php
@@ -96,10 +96,10 @@ get_header();
 
 					<?php if ( get_sub_field('url_icono_ext')): ?>
                      <!-- Si la url es externa, que enlace acepte y use el link externo -->
-                        <a href="<?php the_sub_field('url_icono_ext'); ?>" target="<?php the_sub_field('target_icono'); ?>"><img style="width: 120px;" src="<?php the_sub_field('icono_sec'); ?>"></a>
+                        <a href="<?php the_sub_field('url_icono_ext'); ?>" target="<?php the_sub_field('target_icono'); ?>"><span style="font-size: 50px;" class="<?php the_sub_field('clase_icono'); ?>"></span></a>
                     <?php else :?>
                     <!-- Si no lo es, que muestre un enlace normal interno -->
-                        <a href="<?php the_sub_field('url_icono'); ?>" target="<?php the_sub_field('target_icono'); ?>"><img style="width: 120px;" src="<?php the_sub_field('icono_sec'); ?>"></a>
+                        <a href="<?php the_sub_field('url_icono'); ?>" target="<?php the_sub_field('target_icono'); ?>"><span style="font-size: 50px;" class="<?php the_sub_field('clase_icono'); ?>"></span></a>
                     <?php endif ;?>
 
 					<label class="label-sectIcon" style="font-size: 1.3rem; margin-top: 10px; width: 100%;"><?php the_sub_field('label_icono'); ?></label>
