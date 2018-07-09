cf60c6ba121047534a91806b56f0b988ba90bcac
RubenM <rmoralesl@ids.com.mx>
2018-07-09 16:35:47 -0500

    Ajuste en el componente "chat"
    Se agregaron los botones de llamar, email, chat; editando los archivos chat.php y customSeguros.css

--- template-parts/components/chat.php
+++ template-parts/components/chat.php
@@ -1,9 +1,23 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-2 col-sm-12 pull-right" style="min-height: 0;">
-			<div class="chat-fixed" style="">
+			<!--<div class="chat-fixed" style="">
 				<a class="bbva-coronita_chat " href="" onclick="return openChat();"><span class="chat-fontFamily">Chat</span></a>
-			</div>
+			</div>-->
+			<div class="contiene-boxContact">
+				<a href="#" target="_blank" role="button"><div class="box-contactHelp">
+			        <i class="bbva-coronita_call"></i><br>
+			        <span>Llamar</span>
+			    </div></a>
+			    <a href="" target="_blank" role="button"><div class="box-contactHelp">
+			        <i class="bbva-coronita_mail"></i><br>
+			        <span>Correo</span>
+			    </div></a>
+				<a href="#" target="_blank" role="button"><div class="box-contactHelp">
+			        <i class="bbva-coronita_chat"></i><br>
+			        <span>Chat</span>
+			    </div></a>
+  			</div>
 		</div>
 	</div>
 </div>
