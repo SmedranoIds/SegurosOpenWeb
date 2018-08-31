b7282f2519f8cfdc121dc9add817f252e48f8acd
Aida Rueda Medina <aidarueda.82@gmail.com>
2018-08-30 19:33:57 -0500

    Ajuste de Main Slider para Homepage

---
+++ template-parts/components/bcom-slider.php
@@ -0,0 +1,36 @@
+<link href="../../css/bbva.fonts.min.css" type="text/css" rel="stylesheet">
+<div class="promomarquee__base fadein--fast">
+
+	<!-- <img src="https://www.bancomer.com/content/dam/public-web/bancomer/photos/home-personas-hackaton-marquee.jpg.img.2400.1535569935529.jpg" srcset="https://www.bancomer.com//content/dam/public-web/bancomer/photos/home-personas-hackaton-marquee.jpg.img.320.1535569935529.jpg 320w,https://www.bancomer.com//content/dam/public-web/bancomer/photos/home-personas-hackaton-marquee.jpg.img.768.1535569935529.jpg 768w,https://www.bancomer.com//content/dam/public-web/bancomer/photos/home-personas-hackaton-marquee.jpg.img.960.1535569935529.jpg 960w,https://www.bancomer.com//content/dam/public-web/bancomer/photos/home-personas-hackaton-marquee.jpg.img.1176.1535569935529.jpg 1176w,https://www.bancomer.com//content/dam/public-web/bancomer/photos/home-personas-hackaton-marquee.jpg.img.1600.1535569935529.jpg 1600w,https://www.bancomer.com//content/dam/public-web/bancomer/photos/home-personas-hackaton-marquee.jpg.img.2400.1535569935529.jpg 2400w,https://www.bancomer.com//content/dam/public-web/bancomer/photos/home-personas-hackaton-marquee.jpg.img.2880.1535569935529.jpg 2880w,https://www.bancomer.com//content/dam/public-web/bancomer/photos/home-personas-hackaton-marquee.jpg.img.3200.1535569935529.jpg 3200w"
+	  sizes="100vw" itemprop="image" class="marquee__background__img " alt="imagen"> -->
+	<img src="http://ec2-54-77-102-116.eu-west-1.compute.amazonaws.com/wp-content/uploads/2018/08/HogarSeguro-Homepage-V2.jpg" itemprop="image" class="marquee__background__img " alt="imagen">
+
+
+	<div class="container promomarquee__mask__container">
+		<div class="promomarquee__mask"></div>
+
+		<div class="promomarquee__promobox">
+			<div class="promobox__indicator"></div>
+			<div class="promobox__base">
+				<div class="promobox__base__text">
+					<!-- 
+					<h1 itemprop="name" class="promobox__title">Hackathon BBVA 2018<br>
+					</h1>
+					<div class="promobox__body rte " itemprop="description">
+						Participa con tu equipo y demuestren que son los mejores.
+					</div>
+					<a href="https://hackathonbbva.fastenticket.com/" target="_self" class="promobox__link ">
+						<i class="bbva-icon bbva-coronita_dollar"></i>
+						<span>Obtén un 25% de descuento con el código HACK2018</span>
+					</a> -->
+					<h1 itemprop="name" class="promobox__title">Protege tu casa de niños traviesos con un Seguro de Hogar BBVA Bancomer<br>
+					</h1>
+				</div>
+			</div>
+			<a href="https://cotizadores.segurosbancomer.com.mx/psns_mult_web_psnspublicwebapp_02/hogarSeguroBancomer#/paso1-datos-de-cotizacion" target="_blank" class="promobox__button btn__basic btn__medium-blue">
+				<span class="promobox__button__text">Contratar</span>
+			</a>
+		</div>
+
+	</div>
+</div>
